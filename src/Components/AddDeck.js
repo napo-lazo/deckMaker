d138d0e4ab10e42c671cd28f02b7600a6bd5fa79
napoleon lazo <napoleon.lazo@hotmail.com>
2020-12-13 17:08:21 -0600

    Modified the looks of add deck and some adaptive behavior

--- src/Components/AddDeck.js
+++ src/Components/AddDeck.js
@@ -8,6 +8,26 @@ import React from 'react';
 import Select from '@material-ui/core/Select';
 import Typography from '@material-ui/core/Typography';
 
+// Falta un detalle de la adaptacion horizontal
+// Falta ajustar el espaciado de los elementos
+// Falta tweakear el tama;o del menu
+
+const resizedLargeFont = {
+  fontSize: '2rem'
+};
+
+const resizedMediumFont = {
+  fontSize: '1.5rem'
+};
+
+const resizedSmallFont = {
+  fontSize: '1.25rem'
+};
+
+const test = {
+  maxHeight: '20rem'
+}
+
 class AddDeck extends React.Component {
 
   hearthstoneClasses = [
@@ -55,44 +75,44 @@ class AddDeck extends React.Component {
 
   render() {
     return (
-      <Grid className='addDeck' container item direction='column' justify='space-around' alignItems='center'>
-        <Grid item>
-          <Typography variant='h2'>Add new deck</Typography>
+      <Grid className='addDeck' container item direction='column' justify='space-between' alignItems='center' wrap='nowrap'>
+        <Grid item className='addDeck__Title'>
+          <Typography variant='h1' align='center'>Add new deck</Typography>
         </Grid>
-        <Grid container item direction='column' justify='center' alignItems='center' spacing='10'>
+        <Grid container item direction='column' justify='center' alignItems='center' className='addDeck__Selects' spacing='10'>
           <Grid container item className='addDeck__OptionsContainer' justify='center'>
             <FormControl className='addDeck__OptionsContainer__Option'>
-              <InputLabel>Game</InputLabel>
-              <Select value={this.state.game} onChange={this.handleGameSelect}>
-                <MenuItem value={'hearthstone'}>Hearthstone</MenuItem>
+              <InputLabel style={resizedLargeFont}>Game</InputLabel>
+              <Select value={this.state.game} onChange={this.handleGameSelect} style={resizedLargeFont}>
+                <MenuItem value={'hearthstone'} style={resizedMediumFont}>Hearthstone</MenuItem>
               </Select>
             </FormControl>
             </Grid>
             <Grid container item className='addDeck__OptionsContainer' justify='center'>
             <FormControl className='addDeck__OptionsContainer__Option'>
-              <InputLabel>Format</InputLabel>
-              <Select value={this.state.format} onChange={this.handleFormatSelect}>
-                <MenuItem value={'standard'}>Standard</MenuItem>
-                <MenuItem value={'wild'}>Wild</MenuItem>
+              <InputLabel style={resizedLargeFont}>Format</InputLabel>
+              <Select value={this.state.format} onChange={this.handleFormatSelect} style={resizedLargeFont}>
+                <MenuItem value={'standard'} style={resizedMediumFont}>Standard</MenuItem>
+                <MenuItem value={'wild'} style={resizedMediumFont}>Wild</MenuItem>
               </Select>
             </FormControl>
             </Grid>
-            <Grid container item className='addDeck__OptionsContainer' justify='center'>
+            <Grid container item className='addDeck__OptionsContainer' justify='center' wrap='nowrap'>
             <FormControl className='addDeck__OptionsContainer__Option'>
-              <InputLabel>Class</InputLabel>
-              <Select value={this.state.hClass} onChange={this.handleHClassSelect}>
+              <InputLabel style={resizedLargeFont}>Class</InputLabel>
+              <Select value={this.state.hClass} onChange={this.handleHClassSelect} style={resizedLargeFont} MenuProps={{PaperProps: {style: test}}}>
                 {
                   this.hearthstoneClasses.map((hClass) => 
-                    <MenuItem value={hClass.toLowerCase()}>{hClass}</MenuItem>
+                    <MenuItem value={hClass.toLowerCase()} style={resizedMediumFont}>{hClass}</MenuItem>
                   )
                 }
               </Select>
-              <FormHelperText>Only available for Hearthstone decks</FormHelperText>
+              <FormHelperText style={resizedSmallFont}>Only available for Hearthstone decks</FormHelperText>
             </FormControl>
           </Grid>
         </Grid>
-        <Grid item>
-          <Button variant="contained">Create new deck</Button>
+        <Grid item className='addDeck__Button'>
+          <Button variant="contained" style={resizedSmallFont}>Create new deck</Button>
         </Grid>
       </Grid>
     )
